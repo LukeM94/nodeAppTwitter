4b9112ee41e7b8210a11898a3ae176dc40e93446
LukeM94 <DRU9HEde>
2015-12-17 02:29:50 +0000

    Initial commit

---
+++ retrieve.js
@@ -0,0 +1,50 @@
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+//and our HTTP server
+var http = require('http');
+//setup our port
+var port = process.env.PORT || 1337;
+
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://LukeM94:lmorton-16@ds054288.mongolab.com:54288/mongo';
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Connecting \n');
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        response.write('Connection Made \n');
+        if (err) {
+            response.write('Unable to connect to the mongoDB server. Error:' + err + "\n");
+        } else {
+            //HURRAY!! We are connected. :)
+            response.write('Connection established to' + url +"\n");
+            // Get the documents collection
+            var collection = db.collection('users');
+
+            //We have a cursor now with our find criteria
+            var results = collection.find({age: {$lte:30}});
+            //var results = collection.find({name: 'modulus user'});
+
+
+            //Lets iterate on the result
+            results.each(function (err, result) {
+                if (err) {
+                    response.write(err);
+                } else {
+                    response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
+                }
+                if (result == null) {
+                    response.end('Completed');
+                    db.close();
+                }
+            });
+
+        }
+    });
+
+}).listen(port);
---
+++ twittertest.js
@@ -0,0 +1,25 @@
+var Twitter = require('twitter');
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+var client = new Twitter({
+    consumer_key: 'V9C6Aok8Tcr1aFzhEzfJRDAfE',
+    consumer_secret: '28N666lpWr2xLYYrdTdSm4ZHnQ1vp96SL0pD8WkhkggVAbFgNw',
+    access_token_key: '45614826-gtsSrBU9jPngkeI0RKFv5h40eJOMTUMukZ86GOZ1k',
+    access_token_secret: 'Z0qhznuqPVSTEEuZiARBTh7sCv1EXm0NXAG6acbVzKAH1'
+});
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*'  });
+    //search for 10 tweets containing lolcats
+    client.get('search/tweets', {q: 'lolcats' , count: '10'}, function(error, tweets){
+
+        var json = [];
+        for (var i =0; i< tweets.statuses.length ; i++)
+        {
+            json.push({name: tweets.statuses[i].user.name, text: tweets.statuses[i].text});
+        }
+
+        response.end(JSON.stringify(json));
+    });
+}).listen(port);
